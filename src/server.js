fc282e5a863409169732e6cc60647416a947a1a3
abdulist <dexel7zip@gmail.com>
2025-05-02 18:31:33 +0700

    rollback CameraTest and server.js

--- src/server.js
+++ src/server.js
@@ -33,15 +33,13 @@ app.post('/api/style-transfer', async (req, res) => {
 
     const replicate = new Replicate({ auth: API_TOKEN });
 
-    const { image, style_image, style_prompt, gender } = req.body;
+    const { image, style_image, style_prompt } = req.body;
 
     console.log('ini style image nya:',style_image);
 
     if (!image || !style_image) {
         return res.status(400).json({ success: false, error: "Missing image or style_image" });
     }
-
-    let selectedStylePrompt = '';
 
     const removeBackground = await replicate.run(
         "851-labs/background-remover:a029dff38972b5fda4ec5d75d7d1cd25aeff621d2cf4946a41055d7db66b80bc",
@@ -55,40 +53,8 @@ app.post('/api/style-transfer', async (req, res) => {
           }
         }
     );
+
     const removedBackgroundUrl = removeBackground.url();
-
-    const hijabCheck = {
-        method: "POST",
-        url: "https://serverless.roboflow.com/hijab-nirfi/1",
-        params: {
-            api_key: process.env.ROBOFLOW_API_KEY,
-            image: removedBackgroundUrl
-        }
-    };
-
-    const data = await hijabCheck.json();
-    let isHijab = "";
-    if (data.predictions[0].class_id !== null && (data.predictions[0].class_id === 0 || data.predictions[0].class_id === 1)) {
-        isHijab = " wearing hijab";
-    }
-
-    switch (style_prompt) {
-        case 1:
-            selectedStylePrompt = `a ${gender}${isHijab} with a comic book-style sky with a bright, vivid blue background and scattered white cumulus clouds outlined in black. The scene should include halftone dot patterns, sketch-style brush strokes, and a retro pop art aesthetic. The clouds should have soft, rounded shapes with subtle blue shading and be spread across a dynamic diagonal composition.`;
-            break;
-        case 2:
-            selectedStylePrompt = `a ${gender}${isHijab} with a bold comic book-style sunburst with a bright yellow circular center and sharp yellow rays extending outward. The background should be a vivid teal color with halftone dot patterns and radiating black lines, evoking a vintage pop art or retro comic book vibe. The composition should be symmetrical and eye-catching, with high contrast and clean outlines. keep maintain the face, hair, and head look same with the source of structure image. if the structure image use hijab, then put the hijab.`;
-            break;
-        case 3:
-            selectedStylePrompt = `A ${gender}${isHijab} with a dynamic comic book-style explosion in the gradient caramel with bright orange and yellow bubble, surrounded by dramatic black speed lines. Use a halftone dot pattern in the background with a caramel. The art style should be bold, vibrant, and high-energy, evoking retro pop art and vintage comic aesthetics.`;
-            break;
-        case 4:
-            selectedStylePrompt = `A ${gender}${isHijab} with a vibrant, stylized subway station rendered in a pop-art or comic book aesthetic, with bold green and yellow tones. Two trains are parked on either side of the empty platform, which stretches into a vanishing point in the distance. The ceiling is composed of glowing geometric panels, casting dynamic reflections on the polished floor. The entire scene has a retro-futuristic feel, with heavy linework and halftone textures enhancing the dramatic lighting.`;
-            break;
-        case 5:
-            selectedStylePrompt = `A ${gender}${isHijab} with a futuristic cyberpunk cityscape under a dramatic red sky, with towering dark skyscrapers illuminated by vivid red window lights. The scene is intense and moody, with a graphic comic book anime style featuring bold shadows and a halftone texture. The streets are empty, reflecting streaks of red light, evoking a sense of mystery and dystopia. Keep the face same as reference`;
-            break;
-    }
 
     const output = await replicate.run(
         "fofr/style-transfer:f1023890703bc0a5a3a2c21b5e498833be5f6ef6e70e9daf6b9b3a4fd8309cf0", 
@@ -97,7 +63,7 @@ app.post('/api/style-transfer', async (req, res) => {
                 model: "fast",
                 width: 720,
                 height: 720,
-                prompt: selectedStylePrompt,
+                prompt: style_prompt,
                 style_image: style_image,
                 output_format:"png",
                 output_quality: 80,
@@ -106,9 +72,7 @@ app.post('/api/style-transfer', async (req, res) => {
                 number_of_images: 1,
                 structure_depth_strength: 2,
                 structure_denoising_strength: 0.7
-            }
-        }
-    );
+        }});
 
     const editedImage = output[0].url();
     console.log(editedImage);
